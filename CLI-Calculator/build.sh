513bbb53ad9d2d72ba992481d0b1ed2094faf320
Creitin Gameplays <clton999@outlook.com>
2026-03-16 16:58:18 -0300

    add warn flags

--- CLI-Calculator/build.sh
+++ CLI-Calculator/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p build
-clang++-19 -g -O0 -std=c++23 -stdlib=libc++ -fsanitize=address,undefined -fmodule-file=std=std.pcm CLI-Calculator/main.cpp CLI-Calculator/calculator.cpp std.pcm -o build/calculator
+clang++-19 -g -O0 -std=c++23 -stdlib=libc++ -Wall -Wextra -Wpedantic -fsanitize=address,undefined -fmodule-file=std=std.pcm CLI-Calculator/main.cpp CLI-Calculator/calculator.cpp std.pcm -o build/calculator
