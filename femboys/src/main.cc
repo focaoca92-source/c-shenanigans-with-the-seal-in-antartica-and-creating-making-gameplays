88793e15b5a439135039227239d135ef0dbcc466
Creitin Gameplays <clton999@outlook.com>
2026-04-07 10:59:47 -0300

    while this fucking window is open

--- femboys/src/main.cc
+++ femboys/src/main.cc
@@ -5,45 +5,54 @@
 #include <iostream>
 #include <windows.h>
 #include <regex>
+#include <memory>
 
-int main() {
-    std::println("Running");
-
+std::filesystem::path GetTheMotherFuckingPath(){
     // win32
     wchar_t path[2048];
     GetModuleFileNameW(NULL, path, 2048);
     std::filesystem::path ced = std::filesystem::path(path);
     // go to parent dir
-    std::filesystem::path clean_ced1 = ced.parent_path();
-    std::filesystem::path clean_ced2 = ced.parent_path(); 
+    std::filesystem::path clean_ced = ced.parent_path();
+    return clean_ced;
+}
 
-    sf::RenderWindow thisFuckingWindow(sf::VideoMode({1280, 720}), "testing");
+class startProgram {
+    public:
+        void start(){
+            sf::RenderWindow thisFuckingWindow(sf::VideoMode({800, 600}), "femboy virus");
 
-    // load image
-    std::filesystem::path clean_img_path = clean_ced1 += "\\img.jpg";
-    const sf::Texture thisFuckingTexture(clean_img_path);
-    sf::Sprite thisFuckingSprite(thisFuckingTexture);
+            // load image
+            std::filesystem::path clean_img_path = GetTheMotherFuckingPath() += "\\img.jpg";
+            const sf::Texture thisFuckingTexture(clean_img_path);
+            sf::Sprite thisFuckingSprite(thisFuckingTexture);
 
-    // load font and text
-    std::filesystem::path clean_font_path = clean_ced2 += "\\font.ttf";
-    const sf::Font assFont(clean_font_path);
-    sf::Text thisFuckingText(assFont, "hello cuties", 67);
-    thisFuckingText.setFillColor(sf::Color::Black); // black color
+            // load font and text
+            std::filesystem::path clean_font_path = GetTheMotherFuckingPath() += "\\font.ttf";
+            const sf::Font assFont(clean_font_path);
+            sf::Text thisFuckingText(assFont, "hello cuties", 67);
+            thisFuckingText.setFillColor(sf::Color::Black); // black color
 
-    while (thisFuckingWindow.isOpen()){
-        // event
-        while (const std::optional event = thisFuckingWindow.pollEvent()){
-            if (event->is<sf::Event::Closed>()){
-                thisFuckingWindow.close();
+            while (thisFuckingWindow.isOpen()){
+                // event
+                while (const std::optional event = thisFuckingWindow.pollEvent()){
+                    if (event->is<sf::Event::Closed>()){
+                        thisFuckingWindow.close();
+                    }
+                }
+
+                thisFuckingWindow.clear();
+                thisFuckingWindow.draw(thisFuckingSprite);
+                thisFuckingWindow.draw(thisFuckingText);
+                thisFuckingWindow.display();
             }
         }
+};
 
-        thisFuckingWindow.clear();
-        thisFuckingWindow.draw(thisFuckingSprite);
-        thisFuckingWindow.draw(thisFuckingText);
-        thisFuckingWindow.display();
-    }
-
+int main() {
+    std::println("Running");
+    startProgram femboySpawn;
+    femboySpawn.start();
     std::println("Stopped");
     return 0;
 }
