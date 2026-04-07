8b820c899dd6060dbc4e675b0942fd3e44be8b39
Creitin Gameplays <clton999@outlook.com>
2026-04-07 18:49:15 -0300

    update this shit

--- femboys/src/main.cc
+++ femboys/src/main.cc
@@ -19,49 +19,59 @@ std::filesystem::path GetTheMotherFuckingPath(){
     return clean_ced;
 }
 
-class startProgram {
-    public:
-        startProgram(){
-            sf::RenderWindow thisFuckingWindow(sf::VideoMode({800, 600}), "femboy virus");
-            // load image
-            std::filesystem::path clean_img_path = GetTheMotherFuckingPath() += "\\img.jpg";
-            sf::Texture thisFuckingTexture(clean_img_path);
-            sf::Sprite thisFuckingSprite(thisFuckingTexture);
+void startProgram(){
+    sf::RenderWindow thisFuckingWindow(sf::VideoMode({800, 600}), "femboy virus");
+    std::filesystem::path clean_base_path = GetTheMotherFuckingPath(); // base path
 
-            // load font and text
-            std::filesystem::path clean_font_path = GetTheMotherFuckingPath() += "\\font.ttf";
-            const sf::Font assFont(clean_font_path);
-            sf::Text thisFuckingText(assFont, "hello cuties", 67);
-            thisFuckingText.setFillColor(sf::Color::Black); // black color
+    // load image
+    std::filesystem::path clean_img_path = clean_base_path / "img.jpg";
+    sf::Texture thisFuckingTexture;
 
-            while (thisFuckingWindow.isOpen()){
-                // event
-                _sleep(80); // even the cpu and gpu need a rest (80ms update)
-                while (const std::optional event = thisFuckingWindow.pollEvent()){
-                    if (event->is<sf::Event::Closed>()){
-                        thisFuckingWindow.close();
-                    }
-                }
+    if (!thisFuckingTexture.loadFromFile(clean_img_path)){
+        std::println(stderr, "Couldn't load image\n");
+    };
 
-                // show the stuff in real time
-                thisFuckingWindow.clear();
-                thisFuckingWindow.draw(thisFuckingSprite);
-                thisFuckingWindow.draw(thisFuckingText);
-                thisFuckingWindow.display();
+    sf::Sprite thisFuckingSprite(thisFuckingTexture);
+
+    // load font and text
+    std::filesystem::path clean_font_path = clean_base_path / "font.ttf";
+    sf::Font assFont;
+
+    if (!assFont.openFromFile(clean_font_path)){
+        std::println(stderr, "Couldn't load font file\n");
+    }
+
+    sf::Text thisFuckingText(assFont, "hello cuties", 67);
+    thisFuckingText.setFillColor(sf::Color::Black); // black color
+
+    while (thisFuckingWindow.isOpen()){
+        // even the cpu and gpu need a rest (80ms update).
+        // yes clang++ i know this is deprecated and Windows-only, but it's working so
+        // won't bother with that for now
+        // Linus once said: "don't fix what's not broken" (he probably never said that though)
+        _sleep(80);
+
+        // events
+        while (const std::optional event = thisFuckingWindow.pollEvent()){
+            if (event->is<sf::Event::Closed>()){
+                thisFuckingWindow.close();
             }
         }
-};
+
+        // show the stuff in real time
+        thisFuckingWindow.clear();
+        thisFuckingWindow.draw(thisFuckingSprite);
+        thisFuckingWindow.draw(thisFuckingText);
+        thisFuckingWindow.display();
+    }
+}
 
 int main() {
     std::println("Running");
-    startProgram::startProgram();
+    startProgram();
     std::println("Stopped");
     return 0;
 }
 
 // issues according to "hey claude":
-// 1. misused class
-// 2. _sleep deprecated with best replacement
-// 3. concatenating filepath with += instead of /
-// 4. no error handling for image and font loading
-// do i care?
+// 1. lazy
